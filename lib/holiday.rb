85f1992fd2a56b4579016588e8076a7cef7b0a4d
Alex Engelhart <asengelhart@yahoo.com>
2019-12-16 19:09:49 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,7 +60,8 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, inner_hash|
     puts "#{season.to_s.capitalize}:"
     inner_hash.each do |holiday_sym, supply_array|
-      holiday_name = holiday_sym.to_s 
+      holiday_name = holiday_sym.to_s
+      binding.pry
       holiday_name = holiday_name.split(" ").each{|word| word.capitalize}.join(" ")
       supplies = supply_array.join(", ")
       result_string << "#{holiday_name}: #{supplies}\n"
