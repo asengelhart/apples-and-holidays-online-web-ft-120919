62e33b56d2f3df30e6ba4a917bbbb2e0c36b774f
Alex Engelhart <asengelhart@yahoo.com>
2019-12-16 19:16:37 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -70,7 +70,10 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  result = []
+  holiday_hash.each do |season, inner_hash|
+    
+  end 
 end
 
 
